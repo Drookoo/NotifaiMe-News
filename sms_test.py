35d2dbd3e4f803b0170af0494a2e6ba93df8a682
Justin Schreiber <jschreiber234@gmail.com>
2017-04-09 08:26:23 -0400

    second commit

---
+++ sms_test.py
@@ -0,0 +1,27 @@
+from twilio.rest import Client
+from flask import Flask, request, redirect
+import twilio.twiml
+
+app = Flask(__name__)
+
+# Download the Python helper library from twilio.com/docs/python/install
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = ""
+auth_token = ""
+client = Client(account_sid, auth_token)
+
+
+# message = client.messages("PN215931130033d599b663dda10f665ea1") \
+#  .fetch()
+@app.route('/sms', methods=['POST'])
+def sms():
+    number = request.form['From']
+
+    message = client.api.account.messages.create(to=number,
+                                                  from_="",
+                                                  body="It works now!!")
+    return 'this'
+
+
+if __name__ == '__main__':
+    app.run()
